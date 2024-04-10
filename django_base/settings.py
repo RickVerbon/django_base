e774507f5caf00aa9c1ddee16f5ac150d6e24bb3
Rick Verbon <R.Verbon@tilburguniversity.edu>
2024-04-10 08:56:45 +0200

    Changed dockerfile

--- django_base/settings.py
+++ django_base/settings.py
@@ -52,8 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    #Third party middleware
+    # Third party middleware
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 
 ]
